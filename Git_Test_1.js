924d7cd95340da3c09ebe3a3b438588c5e6e598f
Tiago19480521 <146497077+Tiago19480521@users.noreply.github.com>
2025-12-31 12:34:11 -0300

    All files modified

--- Git_Test_1.js
+++ Git_Test_1.js
@@ -1,1 +1,4 @@
-console.log('Hi, I am here, learning GIT!')
+
+document.getElementById('par_test').innerHTML = 'Hi, I am here, learning GIT!'
+
+document.getElementById('h1_test').innerHTML = 'This is larger text (h1).'
